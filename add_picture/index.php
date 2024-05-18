971ae40bda28889bd4f876979549b7f78f7a03f7
luvumbu <luvumbu.n@gmail.com>
2024-05-18 10:04:22 +0200

    ok

--- add_picture/index.php
+++ add_picture/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<link rel="icon" href="https://pbs.twimg.com/profile_images/1244325575659061249/YjvhVutG_400x400.jpg" type="image/gif" sizes="16x16">
+ 
 
 <body>
     
