c4c8eea1bfcf5777ce42bd2195da85682aef0666
luvumbu <luvumbu.n@gmail.com>
2024-05-19 14:09:23 +0200

    ok

--- class/config.php
+++ class/config.php
@@ -1,6 +1,1 @@
-<?php
-$servername = 'localhost'; // Nom du serveur
-$username = 'root'; // Nom d'utilisateur de la base de données
-$password = 'root'; // Mot de passe de la base de données
-$dbname = 'root'; // Nom de la base de données
-?>
+ 
